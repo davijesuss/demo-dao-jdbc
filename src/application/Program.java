3da663966f296de7d2c6434166bec925098fc775
Davi Jesus <davi7195@gmail.com>
2024-02-18 18:30:55 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departtment;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departtment obj = new Departtment(1, "Books");
+		
+		System.out.println(obj);
+	}
+
+}
